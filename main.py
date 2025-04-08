7735f2fa211f95d4b5403873947107ed8f713cb2
jundam-dannkyle <jundamdannkyle@gmail.com>
2025-04-08 13:50:07 +0800

    initialize the main function

--- main.py
+++ main.py
@@ -15,3 +15,34 @@
 
 # TODO: Implement the main function for the program logic
 # ASSIGNED TO: All
+def main():
+    record_list = []
+
+    while True:
+        print(
+            "\n [1] List All"
+            "\n [2] Add"
+            "\n [3] Update"
+            "\n [4] Delete"
+            "\n [5] Search"
+            "\n [6] Exit"
+        )
+        choice = int(input("Enter your choice: "))
+
+        match choice:
+            case 1:
+                pass
+            case 2:
+                pass
+            case 3:
+                pass
+            case 4: 
+                pass
+            case 5:
+                pass
+            case 6:
+                break
+            case _:
+                print("invalid choice!")
+
+main()
