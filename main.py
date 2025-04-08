3bd6c0fba53f4326fa504f973ac69e8cc3648796
dazo-rollan <olandazo37@gmail.com>
2025-04-08 12:59:35 +0800

    add TODOs for main.py with task assignments

---
+++ main.py
@@ -0,0 +1,17 @@
+# TODO: Implement a function to list all records
+# ASSIGNED TO: Dann Kyle Jundam
+
+# TODO: Implement a function to add a record 
+# ASSIGNED TO: Rollan Dazo
+
+# TODO: Implement a function to update a record
+# ASSIGNED TO: John Albert Olazo
+
+# TODO: Implement a function to delete a record 
+# ASSIGNED TO: Jermaine Raz ehl Agulto
+
+# TODO: Implement a function to search for a record
+# ASSIGNED TO: Joshua Serohijos
+
+# TODO: Implement the main function for the program logic
+# ASSIGNED TO: All
